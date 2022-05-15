042b757fc1f94782ff2daf39f090606a55872dc0
Kemunto Gift <giftoyugi@gmail.com>
2022-05-15 18:05:31 +0300

    add setter tests

--- src/test/java/models/UserTest.java
+++ src/test/java/models/UserTest.java
@@ -30,7 +30,26 @@ class UserTest {
         User testUser = setupUser();
         assertEquals("Finance", testUser.getDepartment());
     }
+    @Test
+    public void setNameSetsCorrectName() throws Exception {
+        User testUser = setupUser();
+        testUser.setName("Thando");
+        assertNotEquals("Anna",testUser.getName());
+    }
+    @Test
+    public void setRoleReturnsCorrectRole() throws Exception {
+        User testUser = setupUser();
+        testUser.setRole("Security");
+        assertNotEquals("Financial officer", testUser.getRole());
+    }
+    @Test
+    public void setDepartmentReturnsCorrectDepartment() throws Exception {
+        User testUser = setupUser();
+        testUser.setDepartment("Welfare");
+        assertNotEquals("Finance", testUser.getDepartment());
+    }
     private User setupUser() {
+        return new User ("Anna", "Financial officer", "Finance");
     }
 
 
