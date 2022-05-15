6498bf4b340d552e53990c43ce473f3c21576d3d
Kemunto Gift <giftoyugi@gmail.com>
2022-05-15 13:08:14 +0300

    add getters and setters

--- src/main/java/models/User.java
+++ src/main/java/models/User.java
@@ -4,5 +4,40 @@ public class User {
     private int id;
     private String name;
     private String role;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+
     private String department;
+
+
 }
