d3772a66c59fdb6aa8e90ae7462dbd59d56c33bc
Kemunto Gift <giftoyugi@gmail.com>
2022-05-15 14:07:51 +0300

    add getters and setter for department

--- src/main/java/models/Department.java
+++ src/main/java/models/Department.java
@@ -6,4 +6,27 @@ public class Department {
     private String description;
     private int totalEmployees;
 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getTotalEmployees() {
+        return totalEmployees;
+    }
+
+    public void setTotalEmployees(int totalEmployees) {
+        this.totalEmployees = totalEmployees;
+    }
 }
--- src/main/java/models/User.java
+++ src/main/java/models/User.java
@@ -6,18 +6,21 @@ public class User {
     private int id;
     private String name;
     private String role;
+    private int departmentId;
 
-    public User(String name, String role, String department) {
+    public User(String name, String role, String department, int departmentId) {
         this.name = name;
         this.role = role;
         this.department = department;
+        this.departmentId = departmentId;
     }
 
-    public User(int id, String name, String role, String department) {
+    public User(int id, String name, String role, String department, int departmentId) {
         this.id = id;
         this.name = name;
         this.role = role;
         this.department = department;
+        this.departmentId = departmentId;
     }
 
     @Override
@@ -25,12 +28,20 @@ public class User {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         User user = (User) o;
-        return id == user.id && name.equals(user.name) && role.equals(user.role) && department.equals(user.department);
+        return id == user.id && departmentId == user.departmentId && name.equals(user.name) && role.equals(user.role) && department.equals(user.department);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(id, name, role, department);
+        return Objects.hash(id, name, role, departmentId, department);
+    }
+
+    public int getDepartmentId() {
+        return departmentId;
+    }
+
+    public void setDepartmentId(int departmentId) {
+        this.departmentId = departmentId;
     }
 
     public int getId() {
