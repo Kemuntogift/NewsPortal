4d622f6b500ed60cec3c4081a6d70e43ddf953e2
Kemunto Gift <giftoyugi@gmail.com>
2022-05-15 17:58:43 +0300

    add getter tests for users

--- src/test/java/models/UserTest.java
+++ src/test/java/models/UserTest.java
@@ -2,6 +2,7 @@ package models;
 
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -14,4 +15,23 @@ class UserTest {
     @AfterEach
     void tearDown() {
     }
+    @Test
+    public void getNameReturnsCorrectName() throws Exception {
+        User testUser = setupUser();
+        assertEquals("Anna", testUser.getName());
+    }
+    @Test
+    public void getRoleReturnsCorrectRole() throws Exception {
+        User testUser = setupUser();
+        assertEquals("Financial officer", testUser.getRole());
+    }
+    @Test
+    public void getDepartmentReturnsCorrectDepartment() throws Exception {
+        User testUser = setupUser();
+        assertEquals("Finance", testUser.getDepartment());
+    }
+    private User setupUser() {
+    }
+
+
 }
