11680d2ddcf16cf9477339e883cfb308623e4b81
Kemunto Gift <giftoyugi@gmail.com>
2022-05-15 19:59:50 +0300

    generate sql2oDepartmentDaoTest class with pre-generated test methods

---
+++ src/test/java/dao/Sql2oDepartmentDaoTest.java
@@ -0,0 +1,62 @@
+package dao;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class Sql2oDepartmentDaoTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void add() {
+    }
+
+    @Test
+    void findById() {
+    }
+
+    @Test
+    void allDepartments() {
+    }
+
+    @Test
+    void deleteDepartmentById() {
+    }
+
+    @Test
+    void allDepartmentEmployees() {
+    }
+
+    @Test
+    void allDepartmentNews() {
+    }
+
+    @Test
+    void updateEmployeeNumber() {
+    }
+
+    @Test
+    void addUserToDepartment() {
+    }
+
+    @Test
+    void deleteEmployeeFromDepartment() {
+    }
+
+    @Test
+    void deleteDeptNewsById() {
+    }
+
+    @Test
+    void deleteAll() {
+    }
+}
