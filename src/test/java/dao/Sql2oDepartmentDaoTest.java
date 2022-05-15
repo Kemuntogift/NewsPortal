33a7676579bc36dcac393ca7d0c23cfda27869e6
Kemunto Gift <giftoyugi@gmail.com>
2022-05-15 20:09:54 +0300

    add before and after logic

--- src/test/java/dao/Sql2oDepartmentDaoTest.java
+++ src/test/java/dao/Sql2oDepartmentDaoTest.java
@@ -3,17 +3,28 @@ package dao;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.sql2o.Connection;
+import org.sql2o.Sql2o;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class Sql2oDepartmentDaoTest {
+    private Connection conn;
+    private Sql2oDepartmentDao departmentDao;
+    private static Sql2oUserDao userDao;
 
     @BeforeEach
-    void setUp() {
+    public void setUp() throws Exception {
+        String connectionString = "jdbc:h2:mem:testing;INIT=RUNSCRIPT from 'classpath:db/create.sql'";
+        Sql2o sql2o = new Sql2o(connectionString, "", "");
+        departmentDao = new Sql2oDepartmentDao(sql2o);
+        userDao = new Sql2oUserDao(sql2o);
+        conn = sql2o.open();
     }
 
     @AfterEach
     void tearDown() {
+        conn.close();
     }
 
     @Test
