b95b5361604dee04f06f6b92ec8ad8c0d4c34311
Kemunto Gift <giftoyugi@gmail.com>
2022-05-15 21:26:26 +0300

    generate test class and pre-set test methods

---
+++ src/test/java/dao/Sql2oUserDaoTest.java
@@ -0,0 +1,42 @@
+package dao;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class Sql2oUserDaoTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void add() {
+    }
+
+    @Test
+    void findById() {
+    }
+
+    @Test
+    void allUsers() {
+    }
+
+    @Test
+    void deleteById() {
+    }
+
+    @Test
+    void deleteAll() {
+    }
+
+    @Test
+    void myNews() {
+    }
+}
